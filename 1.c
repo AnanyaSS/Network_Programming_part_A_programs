c76619c99419ed1f57f8f03a6308f18419cb2db3
ANANYA S <102945315+AnanyaSS@users.noreply.github.com>
2022-06-24 20:58:00 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<string.h>
+void reciever();
+char frames[1024];
+int main()
+{
+    int n,len,i;
+    char buffer[256],length[10];
+    printf("How many frame sdo you want to send");
+    bzero(buffer,256);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter frame\n");
+        scanf("%s",buffer);
+        printf("string length of buffer is %d\n",strlen(buffer));
+        len=strlen(buffer);
+        len=len+1;
+        sprintf(length,"%d",len);
+        strcat(frames,length);
+        strcat(frames,buffer);
+
+
+    }
+    for(i=0;frames[i]!='\0';i++)
+    printf("%c",frames[i]);
+    reciever();
+    return 0;
+}
+void reciever()
+{
+    int i=0,framelen,lpvar;
+    char leninchar;
+    printf("\n\n  This is reciever\n");
+
+    printf("\ndata recieved is %s",frames);
+while(frames[i]!='\0')
+{
+    leninchar=frames[i];
+    framelen=(int) leninchar-(int) '0';
+    printf("\nlength of this frame is %d\n",framelen);
+    printf("\nframe--------->");
+    lpvar=i+framelen;
+    i=i+1;
+    while(i<lpvar)
+    {
+        printf("%c",frames[i++]);
+    }
+    printf("\n");
+}
+}
